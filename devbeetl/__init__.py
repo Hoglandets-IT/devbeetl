011d94cc0a89d90732ea453ea7f9b683a1cad041
Lars <lars@scheibling.se>
2025-04-22 14:38:33 +0200

    Create devbeetl

---
+++ devbeetl/__init__.py
@@ -0,0 +1,72 @@
+import os
+from infisical_client import InfisicalClient, ClientSettings, AuthenticationOptions, UniversalAuthMethod, GetSecretOptions
+from dotenv import load_dotenv
+
+class Devbeetl():
+    client: InfisicalClient
+    
+    addr: str
+    client_id: str
+    client_secret: str
+    project_id: str
+    environment: str
+    
+    @staticmethod
+    def env(name: str, default: str = None):
+        """Get env variable or default"""
+        if name in os.environ:
+            return os.environ[name]
+        if default is not None:
+            return default
+        
+        raise ValueError(f"Environment variable {name} not found")
+    
+    @staticmethod
+    def check_configured():
+        """Check if Devbeetl is configured already"""
+        load_dotenv()
+        
+        if Devbeetl.client is None:
+            Devbeetl.addr = Devbeetl.env("INFISICAL_ADDR")
+            Devbeetl.client_id = Devbeetl.env("INFISICAL_CLIENT_ID")
+            Devbeetl.client_secret = Devbeetl.env("INFISICAL_CLIENT_SECRET")
+            Devbeetl.project_id = Devbeetl.env("INFISICAL_PROJECT_ID")
+            Devbeetl.environment = Devbeetl.env("INFISICAL_ENVIRONMENT")
+            Devbeetl.client = InfisicalClient(
+            ClientSettings(
+                site_url=Devbeetl.addr,
+                auth=AuthenticationOptions(
+                    universal_auth=UniversalAuthMethod(
+                        client_id=Devbeetl.client_id,
+                        client_secret=Devbeetl.client_secret,
+                    )
+                ),
+            )
+        )
+
+    
+    @staticmethod
+    def secret(name: str, path: str = None, project: str = None, environment: str = None):
+        """
+        Returns a secret from Infisical
+        """
+        if project is None:
+            project = Devbeetl.project_id
+            
+        if Devbeetl.client is None:	
+            Devbeetl.check_configured()
+        
+        secret = Devbeetl.client.getSecret(
+            GetSecretOptions.from_dict(
+                {
+                    "projectId": project,
+                    "secretName": name,
+                    "environment": environment,
+                    "path": path,
+                }
+            )
+        )
+        if secret is None:
+            raise ValueError(f"Secret {name} not found in project {project}")
+        
+        return secret.secret_value
---
+++ example.py
@@ -0,0 +1,80 @@
+from devbeetl.devbeetl import Devbeetl
+from beetl.beetl import Beetl, BeetlConfig
+
+
+# Only run fetch and compare, do not update any data
+# Set this via env variable BEETL_DRYRUN
+DRY_RUN = False
+
+
+# Define the configuration
+def make_config():
+    secret_value = Devbeetl.secret(
+        "test_secret",
+        project="12345678-1234-1234-1234-1234567890",
+        environment="dev",
+    )
+    
+    return BeetlConfig(
+        {
+            "sources": [
+                {
+                    "name": "staticsrc",
+                    "type": "Static",
+                    "connection": {
+                        "static": [
+                            {"id": 1, "name": secret_value, "email": "john@test.com"},
+                            {"id": 2, "name": "Jane", "email": "jane@test.com"},
+                            {"id": 3, "name": "Steffen", "email": "steffen@test.com"},
+                        ]
+                    },
+                },
+                {
+                    "name": "staticdst",
+                    "type": "Static",
+                    "connection": {
+                        "static": [
+                            {"id": 1, "name": secret_value, "email": "john@test.com"},
+                            {"id": 4, "name": "James", "email": "jane@test.com"},
+                            {"id": 3, "name": "Stephen", "email": "stephen@test.com"},
+                        ]
+                    },
+                },
+            ],
+            "sync": [
+                {
+                    "source": "staticsrc",
+                    "destination": "staticdst",
+                    "sourceConfig": {},
+                    "destinationConfig": {},
+                    "comparisonColumns": [
+                        {
+                            "name": "id",
+                            "type": "Int64",
+                            "unique": True,
+                        },
+                        {
+                            "name": "name",
+                            "type": "Utf8",
+                        },
+                        {
+                            "name": "email",
+                            "type": "Utf8",
+                        },
+                    ],
+                    "sourceTransformers": [],
+                    "destinationTransformers": [],
+                    "insertionTransformers": [],
+                }
+            ],
+        }
+    )
+
+# Run the sync
+if __name__ == "__main__":
+    dryRun = Devbeetl.env("BEETL_DRYRUN", "true")
+    if dryRun.lower() == "true":
+        DRY_RUN = True
+
+    beetl = Beetl(make_config())
+    beetl.sync(DRY_RUN)
